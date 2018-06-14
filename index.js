22f586c65bf361d8d7fd0e5832d3a654e149dc64
Mahto <sandiper30@gmail.com>
2018-06-14 17:08:04 +0530

    new added files

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ db.once('open',function(){
     console.log("mongo db connection is open");
 });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.listen("4780",function(){
 console.log("server is listening on 4780");
 });
--- mroutes/mroute.js
+++ mroutes/mroute.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var route = express.Router();
 var companies = require('../schema/schema');
+var logger= require("../logger");
+var emitter= require("../events/eventhub");
 route.get('/companies/:name',function(request,reponse){
     let name = request.params.name;
     companies.find({name:name},function(err,data){
@@ -16,9 +18,21 @@ companies.find({name:{$regex:request.params.pattern,$options:'i'}},{_id:0,name:1
 function(err,data){
     if(err)
     reponse.json([]);
+    if(data.length>=50)
+    emitter.emit("more",data.length);
     reponse.json(data);
 
 });
+});
+
+route.put("/empcount/:name",function(request,response){
+    let cname =request.params.name;
+    companies.update({name:cname},{$set:request.body},function(err,data){
+        if(err)
+        response.send({result:"Not Updated"});
+        emitter.emit('update',cname);
+        response.send({result:"successfully Updated"});
+    });
 });
 
 
